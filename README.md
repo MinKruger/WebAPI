23bb52b4fd195ffa184d13e3b24e73a84086d954
Yasmin Kruger <mimi_kruger@hotmail.com>
2020-10-29 08:29:55 -0300

    Project Creation and config

---
+++ README.md
@@ -0,0 +1,8 @@
+# Multinational Web API
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+TODO
+- Explain project
+- Show how clone and run
+- Link to MVC consuming it
---
+++ data/DataContext.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            if (modelBuilder == null)
+                throw new ArgumentNullException(nameof(modelBuilder));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    
+    }
+}
