016276830f43911e9fe9fe8f9090c8b746cf633b
Yasmin Kruger <mimi_kruger@hotmail.com>
2020-11-04 11:48:28 -0300

    Cors setup

--- Startup.cs
+++ Startup.cs
@@ -61,6 +61,11 @@ namespace WebAPI
 
             app.UseRouting();
 
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
